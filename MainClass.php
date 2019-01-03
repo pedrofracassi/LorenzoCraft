3c2d6ae20c84a5b8c0ab119e36481d5fcd54b422
Pedro Fracassi <pedroluizfracassi@gmail.com>
2019-01-03 18:07:10 -0300

    First commit

---
+++ MainClass.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ExamplePlugin;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class MainClass extends PluginBase implements Listener{
+
+	public function onLoad() : void{
+		$this->getLogger()->info(TextFormat::WHITE . "I've been loaded!");
+	}
+
+	public function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getScheduler()->scheduleRepeatingTask(new BroadcastTask($this->getServer()), 120);
+		$this->getLogger()->info(TextFormat::DARK_GREEN . "I've been enabled!");
+	}
+
+	public function onDisable() : void{
+		$this->getLogger()->info(TextFormat::DARK_RED . "I've been disabled!");
+	}
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		switch($command->getName()){
+			case "example":
+				$sender->sendMessage("Hello " . $sender->getName() . "!");
+
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	/**
+	 * @param PlayerRespawnEvent $event
+	 *
+	 * @priority        NORMAL
+	 * @ignoreCancelled false
+	 */
+	public function onSpawn(PlayerRespawnEvent $event) : void{
+		$this->getServer()->broadcastMessage($event->getPlayer()->getDisplayName() . " has just spawned!");
+	}
+}
